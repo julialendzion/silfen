bb3a00d8618a6331b2cf02805e6b6b0a17250a77
Julia Lendzion <juli47g3@stud.kea.dk>
2021-04-26 17:14:56 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+/* document.querySelector(".dropbtn").addEventListener("mouseenter", background);
+document
+  .querySelector(".dropbtn")
+  .addEventListener("mouseleave", backgroundGone);
+
+function background() {
+  console.log("dziala");
+  document.querySelector("#white_bg").classList.remove("hidden");
+}
+
+function backgroundGone() {
+  console.log("dziala");
+  document.querySelector("#white_bg").classList.add("hidden");
+}
+*/
