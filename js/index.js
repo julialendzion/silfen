92f6ef8c9928bee562e20d0284b38c7abf403231
katriinhu <katriinhu@gmail.com>
2021-04-27 16:35:58 +0200

    collections-shop

---
+++ js/index.js
@@ -0,0 +1,40 @@
+function getData() {
+  fetch("https://kea21-6a0c.restdb.io/rest/products", {
+    method: "GET",
+    headers: {
+      "x-apikey": "60339bce5ad3610fb5bb64e6",
+    },
+  })
+    .then((res) => res.json())
+    .then((response) => {
+      showData(response);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+}
+getData();
+
+function showData(data) {
+  console.log(data.slice(0, 4));
+
+  //grab the template
+
+  const template = document.querySelector("#product-template").content;
+  // data.forEach((data) => {
+
+  data.slice(0, 4).forEach((data) => {
+    //clone
+    const copy = template.cloneNode(true);
+
+    //adjust stuff
+    copy.querySelector("h2").textContent = data.name;
+    copy.querySelector("#bagimg").src = data.img_url;
+    copy.querySelector("p").textContent = data.price + `,-`;
+    //copy.querySelector("article img").alt = data.productdisplayname;
+    // copy.querySelector("h3 span").textContent = post.username;
+    // copy.querySelector("a.readmore").href = `article.html?article=${post._id}`;
+    //apend it
+    document.querySelector(".products").appendChild(copy);
+  });
+}
