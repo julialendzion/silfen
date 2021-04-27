28576014ca38e3956f18f5ec1101747099bf8ebd
Julia Lendzion <juli47g3@stud.kea.dk>
2021-04-27 10:20:39 +0200

    menu CSS

--- js/shopsilfen.js
+++ js/shopsilfen.js
@@ -1,15 +1,35 @@
-/* document.querySelector(".dropbtn").addEventListener("mouseenter", background);
-document
-  .querySelector(".dropbtn")
-  .addEventListener("mouseleave", backgroundGone);
-
-function background() {
-  console.log("dziala");
-  document.querySelector("#white_bg").classList.remove("hidden");
+function getData() {
+  fetch("https://kea21-6a0c.restdb.io/rest/products", {
+    method: "GET",
+    headers: {
+      "x-apikey": "60339bce5ad3610fb5bb64e6",
+    },
+  })
+    .then((res) => res.json())
+    .then((response) => {
+      showData(response);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
 }
+getData();
 
-function backgroundGone() {
-  console.log("dziala");
-  document.querySelector("#white_bg").classList.add("hidden");
+function showData(data) {
+  console.log(data);
+
+  //grab the template
+
+  const template = document.querySelector("#product-template").content;
+  data.forEach((data) => {
+    //clone
+    const copy = template.cloneNode(true);
+
+    //adjust stuff
+    copy.querySelector("h2").textContent = data.name;
+    // copy.querySelector("h3 span").textContent = post.username;
+    // copy.querySelector("a.readmore").href = `article.html?article=${post._id}`;
+    //apend it
+    document.querySelector("section").appendChild(copy);
+  });
 }
-*/
