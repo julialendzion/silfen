1eaca1fc3a70d59198def48dbbbf2f6134c17bb0
Julia Lendzion <juli47g3@stud.kea.dk>
2021-05-02 22:41:54 +0200

    filering one at a time works

--- js/shopall.js
+++ js/shopall.js
@@ -1,44 +1,64 @@
-function getData() {
-  fetch("https://kea21-6a0c.restdb.io/rest/products", {
-    method: "GET",
-    headers: {
-      "x-apikey": "60339bce5ad3610fb5bb64e6",
-    },
-  })
-    .then((res) => res.json())
-    .then((response) => {
-      showData(response);
+const url = `https://kea21-6a0c.restdb.io/rest/products`;
+
+//API key
+
+//560263607f98025500000000?s=t
+//The API key
+const options = {
+  headers: {
+    "x-apikey": "60339bce5ad3610fb5bb64e6",
+  },
+};
+
+//chosing the filtering buttons, fetching
+
+//filer function- changing the url
+function filter(filter_type) {
+  console.log("filering", filter_type);
+  document.querySelector("section").innerHTML = "";
+  fetch(
+    `https://kea21-6a0c.restdb.io/rest/products?filter=${filter_type}`,
+    options
+  )
+    .then(function (response) {
+      return response.json();
     })
-    .catch((err) => {
-      console.error(err);
+    .then(function (data) {
+      showData(data);
     });
 }
-getData();
+
+fetch(url, options)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    showData(data);
+  });
 
 function showData(data) {
-  console.log(data);
-
-  //grab the template
-
-  const template = document.querySelector("#product-template").content;
-  data.forEach((data) => {
-    //clone
-    const copy = template.cloneNode(true);
-
-    //adjust stuff
-    copy.querySelector("h2").textContent = data.name;
-    copy.querySelector("#img").src = data.img_url;
-    copy.querySelector("p").textContent = data.price + `,-`;
-    copy.querySelector("a").href = `product.html?id=${data._id}`;
-    //copy.querySelector("article img").alt = data.productdisplayname;
-    // copy.querySelector("h3 span").textContent = post.username;
-    // copy.querySelector("a.readmore").href = `article.html?article=${post._id}`;
-    //apend it
-    document.querySelector("section").appendChild(copy);
-  });
+  console.log("data", data);
+  data.forEach(product);
 }
 
-//filtering menu drop-downs working
+function product(data) {
+  //grab the template
+
+  const template = document.querySelector("template").content;
+  //clone
+  const copy = template.cloneNode(true);
+
+  //adjust stuff
+  copy.querySelector("h2").textContent = data.name;
+  copy.querySelector("#img").src = data.img_url;
+  copy.querySelector("p").textContent = data.price + `,-`;
+  copy.querySelector("a").href = `product.html?id=${data._id}`;
+  //copy.querySelector("article img").alt = data.productdisplayname;
+  // copy.querySelector("h3 span").textContent = post.username;
+  // copy.querySelector("a.readmore").href = `article.html?article=${post._id}`;
+  //apend it
+  document.querySelector("section").appendChild(copy);
+}
 
 document.querySelector("#catbtn").onclick = () => {
   filterMenu();
@@ -68,4 +88,29 @@ function filterMenu3() {
 function filterMenu4() {
   document.querySelector("#price").classList.toggle("hidden");
   document.querySelector("#pribtn span").classList.toggle("rotate");
+}
+
+document.querySelector("#bumbag").onclick = () => {
+  filterBumbags();
+};
+document.querySelector("#crossbody").onclick = () => {
+  filterCrossbody();
+};
+
+function filterBumbags() {
+  if (document.getElementById("bumbag").checked == true) {
+    filter("Mini Bag");
+  } else {
+    filter("");
+    console.log("");
+  }
+}
+
+function filterCrossbody() {
+  if (document.getElementById("crossbody").checked == true) {
+    filter("Crossbody Bag");
+  } else {
+    filter("");
+    console.log("");
+  }
 }
